94de9170ec0048f698afc550faf45fa866465138
BariyDev <barinov2702@gmail.com>
2025-09-21 21:37:34 +0300

    feat: implement countVowels function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -307,8 +307,15 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'];
+  let count = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    for (let j = 0; j < 13; j += 1) {
+      if (str[i] === vowels[j]) count += 1;
+    }
+  }
+  return count;
 }
 
 /**
