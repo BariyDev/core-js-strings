df54ba25ca16f44a582afc005211da17aa7a65fb
BariyDev <barinov2702@gmail.com>
2025-09-21 14:17:30 +0300

    feat: implement sumOfCodes function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -189,8 +189,16 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  if (!(typeof str === 'string' || str instanceof String)) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    const code = str.charCodeAt(i);
+    sum += code;
+  }
+  return sum;
 }
 
 /**
