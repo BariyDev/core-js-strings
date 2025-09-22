234b719318e7e42451af4ffbf155d1e68c177c5b
BariyDev <barinov2702@gmail.com>
2025-09-22 15:01:17 +0300

    feat: implement invertCase function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -383,8 +383,12 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  return str.replace(/[a-z]/gi, (char) => {
+    return char === char.toUpperCase()
+      ? char.toLowerCase()
+      : char.toUpperCase();
+  });
 }
 
 /**
